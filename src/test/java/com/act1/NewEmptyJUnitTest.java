cba5d222b732a1a3f21f7207409aebf59eb8ded0
Alysson Cordeiro <Alysson Cordeiro@PC-ALYSSON>
2021-05-03 18:32:01 -0300

    Revert "Criação de Testes Testes do 1º Quadro"
    This reverts commit 35da3fbbd5cbad65e72c876f1264830f4f434cd7.

--- src/test/java/com/act1/NewEmptyJUnitTest.java
+++ src/test/java/com/act1/NewEmptyJUnitTest.java
@@ -24,10 +24,5 @@ public class NewEmptyJUnitTest {
         assertEquals(3.07, s.MOPercentual(30.7));//Calculo do M.O%
         assertEquals(17.848837209302324, s.calculaCarbono(3.07)); //Calculo do Carbono
     }
-    
-    @Test
-    public void test02(){
-        
-    }
 
 }
